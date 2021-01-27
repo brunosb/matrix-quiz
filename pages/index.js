e3563fc6ca74d8d1a09545548bf613b6644d4470
Bruno Barbosa <barbosa.developer@gmail.com>
2021-01-27 17:01:12 -0300

    Input e button components

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,8 @@ import QuizLogo from '../src/components/QuizLogo';
 import BackgroundMatrix from '../src/components/BackgroundMatrix';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -45,19 +47,23 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+
+            <form onSubmit={onSubimitForm}>
+              <Input
+                name=""
+                placeholder="Digite seu nome"
+                onChange={onChangeName}
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}
+              </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
-          <Widget.Content>
-            <form onSubmit={onSubimitForm}>
-              <input placeholder="Digite seu nome" onChange={onChangeName} />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar
-                {name}
-              </button>
-            </form>
-          </Widget.Content>
+          <Widget.Content />
         </Widget>
         <Footer />
       </QuizContainer>
